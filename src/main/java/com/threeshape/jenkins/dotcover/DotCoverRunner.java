a3af6c254419b6df0e0b72a186744e592c996d9d
Rasmus Jelsgaard <rasmus.jelsgaard@3shape.com>
2018-12-17 11:19:19 +0100

    Added basic class to get started. Updated gitignore.

---
+++ src/main/java/com/threeshape/jenkins/dotcover/DotCoverRunner.java
@@ -0,0 +1,20 @@
+package com.threeshape.jenkins.dotcover;
+
+import hudson.Plugin;
+
+import java.util.logging.Logger;
+
+public class DotCoverRunner extends Plugin {
+
+    public DotCoverRunner()
+    {
+        
+    }
+
+    private static final Logger log = Logger.getLogger(DotCoverRunner.class.getName());
+
+    public void start()
+    {
+        log.info("Starting DotCover plugin");
+    }
+}
